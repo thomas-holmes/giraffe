5de0e7528dfc2e550624f467cf84d77b568a3d9c
Thomas Holmes <shadox@gmail.com>
2018-09-24 16:31:21 -0400

    Add some specific metadata decoding

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"bytes"
 	"encoding/binary"
 	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
 )
+
+// 8922 expected raw resolution of 6000x4000, adobe rgb, depth 8bit, 300ppi
 
 func main() {
 	log.Println("Giraffe loves pictures ��")
@@ -26,6 +30,28 @@ func main() {
 		log.Panicln(err)
 	}
 	log.Println(rawHeader)
+
+	jpgBytes := make([]byte, rawHeader.JpgLength)
+	file.ReadAt(jpgBytes, int64(rawHeader.JpgOffset))
+
+	if err := ioutil.WriteFile(outPath, jpgBytes, 0666); err != nil {
+		log.Panicln(err)
+	}
+
+	// file.Seek(int64(rawHeader.CfaHeaderOffset), 0)
+
+	metaBytes := make([]byte, rawHeader.CfaHeaderLength)
+	_, err = file.ReadAt(metaBytes, int64(rawHeader.CfaHeaderOffset))
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	cfaHeader, err := ReadCFAHeader(bytes.NewBuffer(metaBytes))
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	log.Println(cfaHeader)
 
 }
 
@@ -64,6 +90,70 @@ type RAWHeader struct {
 
 	CfaOffset uint32
 	CfaLength uint32
+}
+
+type CFAHeader struct {
+	NumRecords uint32
+
+	Records []CFARecord
+}
+
+type CFARecord struct {
+	TagID uint16
+	Size  uint16
+
+	Data interface{}
+}
+
+func (c CFARecord) String() string {
+	return fmt.Sprintf(
+		`
+		TagID: %#x
+		Size: %d
+		Data: %v
+		`,
+		c.TagID,
+		c.Size,
+		c.Data,
+	)
+}
+
+func ReadCFAHeader(hReader io.Reader) (CFAHeader, error) {
+	var header CFAHeader
+	if err := binary.Read(hReader, binary.BigEndian, &header.NumRecords); err != nil {
+		return header, err
+	}
+
+	for i := uint32(0); i < header.NumRecords; i++ {
+		var rec CFARecord
+		if err := binary.Read(hReader, binary.BigEndian, &rec.TagID); err != nil {
+			return header, err
+		}
+		if err := binary.Read(hReader, binary.BigEndian, &rec.Size); err != nil {
+			return header, err
+		}
+
+		data := make([]byte, rec.Size)
+		if err := binary.Read(hReader, binary.BigEndian, data); err != nil {
+			return header, err
+		}
+
+		switch rec.TagID {
+		case RAFTagImgHeightWidth, RAFTagOutputHeightWidth, RAFTagImgTopLeft, RAFTagSensorDimension:
+			h, w := binary.BigEndian.Uint16(data[:2]), binary.BigEndian.Uint16(data[2:])
+			rec.Data = []uint16{h, w}
+		case RAFTagRawInfo:
+			rawProps := binary.BigEndian.Uint32(data)
+			compressed := ((rawProps & 0xFF0000) >> 16) & 8
+			rec.Data = compressed
+		default:
+			continue
+		}
+
+		header.Records = append(header.Records, rec)
+	}
+
+	return header, nil
 }
 
 func (r RAWHeader) String() string {
@@ -119,3 +209,11 @@ func init() {
 		outPath = filepath.Join(tempDir, "output.jpg")
 	}
 }
+
+const (
+	RAFTagSensorDimension   = 0x100
+	RAFTagImgTopLeft        = 0x110 // Origin
+	RAFTagImgHeightWidth    = 0x111 // Full Dimensions?
+	RAFTagOutputHeightWidth = 0x121 // Cropped Dimension?
+	RAFTagRawInfo           = 0x130
+)
---
+++ pkg/exif/fields.go
@@ -0,0 +1,31 @@
+package exif
+
+import (
+	"encoding/binary"
+	"io"
+	"log"
+)
+
+func ValFromTagID(r io.Reader, tagID uint16) (interface{}, bool) {
+	switch tagID {
+	case 0x100: // Image Width
+		return readUint32(r), true
+	case 0x101: // Image HEight
+		return readUint32(r), true
+	case 0xc000: // Fuji RAF data https://www.sno.phy.queensu.ca/~phil/exiftool/TagNames/FujiFilm.html#RAFData
+		return nil, false
+	default:
+		return nil, false
+	}
+}
+
+func readUint32(r io.Reader) interface{} {
+	dat := make([]byte, 4)
+
+	_, err := r.Read(dat)
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	return binary.BigEndian.Uint32(dat)
+}
